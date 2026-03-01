fcb6cc03b8b071c38e5be380adee167d53a6b4ee
maelho <51026438+maelho@users.noreply.github.com>
2026-03-01 13:53:35 -0400

    add router

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite"
+import { devtools } from "@tanstack/devtools-vite"
+import tsconfigPaths from "vite-tsconfig-paths"
+import { tanstackStart } from "@tanstack/react-start/plugin/vite"
+import viteReact from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
+import { nitro } from "nitro/vite"
+
+export default defineConfig({
+  plugins: [
+    devtools(),
+    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+    tsconfigPaths({ projects: ["./tsconfig.json"] }),
+    tailwindcss(),
+    tanstackStart({
+      srcDirectory: "src",
+      router: {
+        routesDirectory: "app",
+      },
+    }),
+    viteReact({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+  ],
+})
