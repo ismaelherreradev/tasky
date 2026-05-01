ce1c713f40ae5ce067b9c0d70bc93ef1eefb8429
maelho <51026438+maelho@users.noreply.github.com>
2026-05-01 15:53:43 -0400

    remove next

--- src/old/server/api/routers/logs/logs.schema.ts
+++
@@ -1,9 +0,0 @@
-import type { z } from "zod";
-
-import { createEntitySchema, idSchema } from "../../shared/schema-utils";
-
-export const ZGetAuditLogs = createEntitySchema({
-  id: idSchema,
-});
-
-export type TGetAuditLogs = z.infer<typeof ZGetAuditLogs>;
